7901a6f8fe4231c6f3016cd4d6478885b6dac9e1
Sravanthi-Bareddy <165849859+Sravanthi-Bareddy@users.noreply.github.com>
2024-05-10 11:57:45 -0500

    Updated some changes

--- src/components/LightDarkMode/index.js
+++ src/components/LightDarkMode/index.js
@@ -2,26 +2,29 @@ import {Component} from 'react'
 import './index.css'
 
 class LightDarkMode extends Component {
-  state = {lightMode: True}
+  state = {
+    lightMode: true,
+  }
+  
   changeMode = () => {
-    this.setState(prevState => ({lightMode: prevState.lightMode}))
+    this.setState(prevState => ({
+      lightMode: !prevState.lightMode,
+    }))
   }
-  getButtonText = () => {
-    const {lightMode} = this.state
-
-    return lightMode ? 'Dark Mode' : 'Light Mode'
-  }
+  
 
   render() {
-    const {result} = this.getButtonText()
+    const {lightMode} = this.state
+    const className = lightMode ? 'light-mode' : 'dark-mode'
+    const buttonText = lightMode ? 'Dark Mode' : 'Light Mode'
 
     return (
       <div className="bg-container">
         <div className="mode-change-container">
           <h1 className="mode-change-heading">click To Change Mode</h1>
           <div>
-            <button type="button" onClick={this.changeMode}>
-              {result}
+            <button type="button" onClick={this.changeMode} className={`className`}>
+              {buttonText}
             </button>
           </div>
         </div>
